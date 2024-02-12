91be7aaf33cd81f07724e22b85882cbf61d3ab21
RyanBeGell <Ryanbegell@outlook.com>
2024-02-12 18:04:39 -0500

    Add CORS headers to response

--- ContactFormHandler.js
+++ ContactFormHandler.js
@@ -36,6 +36,12 @@ exports.handler = async (event) => {
     await SES.sendEmail(params).promise();
     return {
       statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Credentials': true,
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET',
+      },
       body: JSON.stringify({ message: 'Email sent successfully' }),
     };
   } catch (error) {
